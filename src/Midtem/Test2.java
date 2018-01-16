15223ccf851a9f9aadb77b9e2983aeade5ea31f6
jim055 <thasawan_13@hotmail.com>
2018-01-16 11:28:23 +0700

    MidtermSalary

--- src/Midtem/Test2.java
+++ src/Midtem/Test2.java
@@ -1,4 +1,53 @@
 package Midtem;
 
+import java.util.Scanner;
+
 public class Test2 {
-}
+    static Scanner scanner = new Scanner(System.in);
+    public static void main (String[] args){
+        String id = getID();
+        String name = getName();
+        String salary = getSalary();
+        String overTime = getOverTime();
+        String calculateTax = calculateTax();
+        String showDataInfo = showDataInfo();
+
+
+    }//main
+    private static String getID(){
+        System.out.print("Enter employee id: ");
+        return scanner.nextLine();
+    }
+
+    private static String getName(){
+        System.out.print("Enter employee name: ");
+        return scanner.nextLine();
+
+    }
+
+    private static String getSalary(){
+        System.out.print("Enter employee salary: ");
+        return scanner.nextLine();
+    }
+
+    private static String getOverTime(){
+        System.out.print("Enter employee overtime: ");
+        return scanner.nextLine();
+    }
+
+    private static String calculateTax(){
+        System.out.print("Enter employee calculatetax: ");
+        return scanner.nextLine();
+
+    }
+
+    private static String showDataInfo(){
+        System.out.print("Enter employee showdatainfo: ");
+        return scanner.nextLine();
+
+
+
+
+    }
+
+}//class
